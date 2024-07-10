5b7e6703de6c8a0a942cdc44bd4091d235deb92c
NguyenQuocTrungg <21130588@st.hcmuaf.edu.vn>
2024-07-10 15:12:30 +0700

    đăng nhÃập bÄằng gg

--- src/main/java/Controller/LoginGoogleHandler.java
+++ src/main/java/Controller/LoginGoogleHandler.java
@@ -3,22 +3,18 @@ package Controller;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
+import DAO.CustomerDao;
+import Model.Customer;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.client.fluent.Form;
 
-import DAO.CustomerDao;
-import Model.Customer;
-
-@WebServlet("/LoginGoogleHandler" )
+@WebServlet("/LoginGoogleHandler")
 public class LoginGoogleHandler extends HttpServlet {
 
 	/**
@@ -31,15 +27,39 @@ public class LoginGoogleHandler extends HttpServlet {
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String code = request.getParameter("code");
-		String accessToken = getToken(code);
-		UserGoogleDto user = getUserInfo(accessToken);
-		//System.out.println(user);
+		if (code != null) {
+			String accessToken = getToken(code);
+			UserGoogleDto user = getUserInfo(accessToken);
+			CustomerDao customerDao = CustomerDao.GetInstance();
+			Customer customer = customerDao.getCustomerByEmail(user.getEmail());
 
+			if (customer == null) {
+				// If user does not exist, create a new one
+				customer = new Customer();
+				customer.setUsername(user.getName());
+				customer.setEmail(user.getEmail());
+				customer.setNumberPhone("");  // Assuming number phone is not available from Google info
+				customer.setPassword(""); // Password should be handled securely if required
 
+				customerDao.insert(customer);
+			}
+
+			// Set user in session
+			request.getSession().setAttribute("customer", customer);
+			// Redirect to home page or the desired page
+			//response.sendRedirect("index.jsp");
+			request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request,response);
+
+		} else {
+			// Redirect to login page if code is not available
+			//response.sendRedirect("login.jsp");
+			request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request,response);
+
+		}
 	}
 
 	public static String getToken(String code) throws ClientProtocolException, IOException {
-		// call api to get token
+		// Call API to get token
 		String response = Request.Post(Constants.GOOGLE_LINK_GET_TOKEN)
 				.bodyForm(Form.form().add("client_id", Constants.GOOGLE_CLIENT_ID)
 						.add("client_secret", Constants.GOOGLE_CLIENT_SECRET)
@@ -55,47 +75,24 @@ public class LoginGoogleHandler extends HttpServlet {
 	public static UserGoogleDto getUserInfo(final String accessToken) throws ClientProtocolException, IOException {
 		String link = Constants.GOOGLE_LINK_GET_USER_INFO + accessToken;
 		String response = Request.Get(link).execute().returnContent().asString();
-
 		UserGoogleDto googlePojo = new Gson().fromJson(response, UserGoogleDto.class);
-
 		return googlePojo;
 	}
 
-	// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the +
-	// sign on the left to edit the code.">
-	/**
-	 * Handles the HTTP <code>GET</code> method.
-	 * @param request servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException if an I/O error occurs
-	 */
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		processRequest(request, response);
 	}
 
-	/**
-	 * Handles the HTTP <code>POST</code> method.
-	 * @param request servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException if an I/O error occurs
-	 */
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		processRequest(request, response);
 	}
 
-	/**
-	 * Returns a short description of the servlet.
-	 * @return a String containing servlet description
-	 */
 	@Override
 	public String getServletInfo() {
 		return "Short description";
-	}// </editor-fold>
-
+	}
 }
--- src/main/java/util/Email.java
+++ src/main/java/util/Email.java
@@ -43,9 +43,9 @@ public class Email {
 			//người gửi
 			msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail,false));
 			//Tiêu đề email
-			msg.setSubject("XEM MẬT KHẨU Ở ĐÂY");
+			msg.setSubject("XEM MÃ CODE Ở ĐÂY","UTF-8");
 			//nội dung
-			msg.setText("Mật khẩu của bạn là: "+message, "UTF-8");
+			msg.setText("Mã Code của bạn là: "+message, "UTF-8");
 			//gửi mail
 			Transport.send(msg);
 		} catch (MessagingException e) {
